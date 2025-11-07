34292694ebb4b1671d3b5b640fec110fba3ef591
Abdul-Wahab-Awan <167404782+Abdul-Wahab-Awan@users.noreply.github.com>
2025-11-08 02:21:40 +0500

    Add files via upload

---
+++ PracticeProgram-Variables-Datatypes.py
@@ -0,0 +1,11 @@
+print("\t\t PROBLEM!! \t\t")
+"""Calculate area of a rectangle having length of 15 cm and width of 10cm 
+Find the Area of Rectangle """
+#Here we acknowledge what to we will multiple len and wid and we will get the area 
+# but We wil understand this in a programmer way
+print("\t\t Stepwise !! \t\t\n one one variable for 15 and 10 and Also which we have to find Area it will stored ")
+print("\t at  last * both variable to get ans  !! \t\t")
+x1=15;
+x2=10;
+Area=x1*x2; #15*10
+print("Area of the rectangle is: ",Area);
