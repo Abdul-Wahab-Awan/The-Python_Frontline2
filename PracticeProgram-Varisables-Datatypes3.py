5b8b2ca03b89837cf0142f15bde424b706561991
Abdul-Wahab-Awan <167404782+Abdul-Wahab-Awan@users.noreply.github.com>
2025-11-09 00:53:44 +0500

    Add files via upload

---
+++ PracticeProgram-Varisables-Datatypes3.py
@@ -0,0 +1,15 @@
+#it will be the last practice program of today topic Variables and Datatypes. 
+print("\t\t\t\tProblem!!\t\t\n \tConvert Degree Celsius Temperature to fahrenhiet and vice verse");
+"""The most important thing or we can say this to solve this kind 
+of problems the one and only important thing is to know the formulas 
+which will be used in the required problem """
+#like we solved our previous problem of area where we about the formula so we multiplied it
+#Same like that we have to the formulas
+F=102;
+ 
+# celsius C=5*(F-32)/9
+# fahrenhiet F=(9/5)*C+32
+#the formulas which will be used in it
+C=5*(F-32)/9;
+print(C);
+#if we want the temperature of fahrenhiet then vice verse
